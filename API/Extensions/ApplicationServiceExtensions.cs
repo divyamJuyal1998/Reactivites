895f115ebf7fd7a7ad2ddefbaa64f168ee0dc8fd
Divyam <divyamj980fficial.com>
2021-05-03 13:22:47 +0530

    test

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -15,7 +15,7 @@ namespace API.Extensions
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }) ;
             });
             services.AddDbContext<DataContext>(opt =>
             {
